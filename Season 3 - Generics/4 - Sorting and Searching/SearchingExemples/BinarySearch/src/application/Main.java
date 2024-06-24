c34b0eced7126999cc26b06af078869a56451bf0
João Bertholino <joaobertholino964@gmail.com>
2024-06-23 23:24:43 -0300

    Season 3

--- Season 3 - Generics/4 - Sorting and Searching/SearchingExemples/BinarySearch/src/application/Main.java
+++ Season 3 - Generics/4 - Sorting and Searching/SearchingExemples/BinarySearch/src/application/Main.java
@@ -8,41 +8,20 @@ public class Main {
 		int[] numbers = {2, 10, 16, 34, 37, 56, 57, 76, 81, 83, 85};
 
 		displayNumbers(numbers);
-
 		try {
-			binarySearchOne(numbers, 76);
-			binarySearchTwo(numbers, 76);
+			if (binarySearchOne(numbers, 77)) System.out.println("Valor encontrado!");
 		} catch (ElementFound | ElementNotFound e) {
 			System.err.println(e.getMessage());
 		}
 	}
 
-	public static void binarySearchOne(int[] numbers, int value) {
+	public static boolean binarySearchOne(int[] numbers, int value) {
 		int low = 0;
 		int high = numbers.length - 1;
 		while (high >= low) {
 			int middle = (low + high) / 2;
 			if (numbers[middle] == value) {
-				System.out.println("The number \"" + value + "\" is at position " + middle + " in the list");
-				return;
-			}
-			if (numbers[middle] < value) {
-				low = middle + 1;
-			}
-			if (numbers[middle] > value) {
-				high = middle - 1;
-			}
-		}
-		System.out.println("The number \"" + value + "\" is not in the list");
-	}
-
-	public static void binarySearchTwo(int[] numbers, int value) {
-		int low = 0;
-		int high = numbers.length - 1;
-		while (high >= low) {
-			int middle = (low + high) / 2;
-			if (numbers[middle] == value) {
-				throw new ElementFound("The number \"" + value + "\" is at position " + middle + " in the list");
+				return true;
 			}
 			if (numbers[middle] < value) {
 				low = middle + 1;
